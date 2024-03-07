47629c93d9116e8c159a4a5d789fca25a4137693
João Lúcio <jpedro.boavida@gmail.com>
2024-03-07 14:28:34 +0000

    avatar1

--- sketch.js
+++ sketch.js
@@ -1,11 +1,66 @@
 function setup() {
-  createCanvas(1280, 1024);
-  background(153);
-  stroke(0);
-  strokeWeight(2);
-  fill(237, 28, 36);
-  circle(30, 30, 40);
-  line(30, 30, 1280 / 2, 1024 / 2);
+  createCanvas(1920, 1080);
 }
 
-function draw() {}
+function draw() {
+  background(65,110,255);
+  fill (75,155,175);
+  stroke(2);
+  rect(1920/2,1080/2,40,80);
+  fill (75,155,175);
+  stroke(2);
+  rect(920,540,40,80);
+  fill (50,50,10);
+  stroke(2);
+  rect(920,620,40,10);
+  fill (50,50,10);
+  stroke(2);
+  rect(960,620,40,10);
+  fill (255,25,25);
+  stroke(2);
+  rect(920,430,80,110);
+  fill (255,25,25);
+  stroke(2);
+  rect(890,430,30,90);
+  fill (255,25,25);
+  stroke(2);
+  rect(1000,430,30,90);
+  rect(920,430,80,110);
+  fill (247,181,121);
+  stroke(2);
+  rect(890,520,30,20);
+  fill (247,181,121);
+  stroke(2);
+  rect(1000,520,30,20);
+  fill (247,181,121);
+  stroke(2);
+  rect(920,350,80,80);
+  fill (30,110,40);
+  stroke(2);
+  rect(0,630,1920,1080);
+  fill(255)
+  stroke(5);
+  circle(940,380,30);
+  fill(255)
+  stroke(5);
+  circle(980,380,30);
+  fill(0)
+  stroke(5);
+  circle(980,383,15);
+  fill(0)
+  stroke(5);
+  circle(940,383,15);
+  noFill()
+  stroke(5);
+  circle(980,380,40);
+  noFill()
+  stroke(5);
+  circle(940,380,40);
+  noFill()
+  stroke(5);
+  circle(980,380,40);
+  noFill()
+  stroke(5);
+  circle(940,380,40);
+  
+}
